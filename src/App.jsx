d48ee0bc7b19e9037387d2c7170b1c3db4b755d9
00buku <clydescottwilliamsjr@gmail.com>
2023-02-03 08:34:00 -0600

    removed dashboard for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import UserDashboard from "./pages/UserDashboard/UserDashboard";
+
 import BookingPage from "./pages/Booking/BookingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import HomePage from "./pages/HomePage/HomePage";
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/dashboard" element={<UserDashboard />} />
+
       <Route path="/booking" element={<BookingPage />} />
       <Route path="/signup" element={<SignUpPage handleSignUpOrLogin={handleSignUpOrLogin}/>} />
       <Route path="/" element={<HomePage />} />
