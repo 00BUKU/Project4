b57b92744803a491e9c312a0fbdd0e1a7dfdebe5
00buku <clydescottwilliamsjr@gmail.com>
2023-02-05 21:02:37 -0600

    styling changes

--- src/pages/PodsPage/PodsPage.jsx
+++ src/pages/PodsPage/PodsPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import tokenService from "../../utils/tokenService";
+import "./PodsPage.css";
 
 const PodsPage = () => {
   const navigate = useNavigate();
